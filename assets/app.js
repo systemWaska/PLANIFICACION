9027a893d1274b850913d1f30f179ca3a0f0276d
systemWaska <sistemaswaska@gmail.com>
2025-12-29 15:33:58 -0500

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,7 @@
  */
 
 const APPS_SCRIPT_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbw2TYdikRhGWefhv6ijzG_pz_vlULRWMihjlMrgtlVzvq6nhYot1101G3Ict_XToPWrLQ/exec";
-const API_TOKEN = "planificacion-2026-segura"; // mismo que en Apps Script
+const API_TOKEN = "planificacion-2026"; // mismo que en Apps Script
 
 const $ = (sel) => document.querySelector(sel);
 
