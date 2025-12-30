10256687e86cdfd793d567d17f9ba5e7af1fcbba
systemWaska <sistemaswaska@gmail.com>
2025-12-30 09:10:19 -0500

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,3 @@
 // Edita SOLO esta variable cuando cambies de WebApp en Apps Script
 // (Implementar > Nueva implementación > copiar URL del Web App)
-window.APPS_SCRIPT_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbw2TYdikRhGWefhv6ijzG_pz_vlULRWMihjlMrgtlVzvq6nhYot1101G3Ict_XToPWrLQ/exec";
+window.APPS_SCRIPT_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxRYj6GaB8O7q-reEmLTPuZsoDDNQo9Gp_MDlJaFTJ-MiCF5vZ5DRk7gptwDYjA85G4UQ/exec";
