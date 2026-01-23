8226bbe1655a87fbe928ad0c4fe999ba91266309
systemWaska <sistemaswaska@gmail.com>
2026-01-23 14:21:31 -0500

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -3,7 +3,7 @@ window.APP_CONFIG = {
   // URL del Web App de Google Apps Script (Implementar -> Aplicación web)
   SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxRYj6GaB8O7q-reEmLTPuZsoDDNQo9Gp_MDlJaFTJ-MiCF5vZ5DRk7gptwDYjA85G4UQ/exec",  // Asegúrate de reemplazar esto con la URL correcta de tu Web App
   // (Opcional) ID del Spreadsheet. Si está vacío, puedes pasar ?sid=... en la URL del sitio.
-  SPREADSHEET_ID: ""  
+  SPREADSHEET_ID: "1-fgZyArdMd0zdqC9j__AL1Td_q5KkSBjaSUMyzvESrY"  
 };
 
 // Compatibility with older code
